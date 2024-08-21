fdc4405b7ddb9859ffaa476d8d7b55af6c0c39d4
Unique Neupane <khemrajneupane111@gmail.com>
2024-08-21 13:56:48 +0545

    todao list code upload

---
+++ resources/views/create.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>
+    </title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container mt-3 py-5 "style="font-family:'Times New Roman', Times, serif;"">
+    <h1 class="h3 mb-2 text-center">Create To Do List </h1>
+    <div class="card mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Create ToDo list
+            <a  class =" btn btn-primary ml-5" href="{{ route('list') }}">List </a>
+            </h6>
+        </div>
+        <div class="card-body">
+            <form action="" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <label> User Name</label>
+                    <input type="text" name="username" placeholder="Enter User Name" class="form-control" value="{{old('username')}}">
+                  {{-- @include('backend.includes.form_element_error',['field'=>'username']) --}}
+                </div>
+                <div class="form-group">
+                    <label> Title</label>
+                    <input type="text" name="title"  class="form-control" value="{{old('title')}}">
+                    {{-- @include('backend.includes.form_element_error',['field'=>'title']) --}}
+                </div>
+
+                <div class="form-group">
+                    <label>Description</label>
+                   <textarea name="" id="" cols="5" rows="5" class="form-control"></textarea>
+                    {{-- @include('backend.includes.form_element_error',['field'=>'favicon']) --}}
+                </div>
+
+
+                <div class="form-group">
+                  <input type="submit" value="Craete" class="btn btn-success">
+                    <input type="reset" value="Reset" class="btn btn-danger">
+                </div>
+            </form>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+</html>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-3 "style="font-family:'Times New Roman', Times, serif;">
+        <h1 class="h4 mb-2 text-center  font-weight-bold" >To DO APP</h1>
+        <div class="card mb-4">
+            <div class="card-header">
+                <h6 class="m-0 font-weight-bold text-primary text-center">TO Do List</h6>
+                <a  class =" btn btn-primary ml-5" href="">Create </a>
+            </div>
+            <div class="card-body">
+
+                <div class="table-responsive">
+                    <table class="table table-bordered table-sm" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>User Name</th>
+                                <th>Title</th>
+                                <th>Description</th>
+                                <th>Create Date</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                               <td>1</td>
+                               <td>Unique Neupane</td>
+                               <td>Online News Portal</td>
+                               <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, reprehenderit.</td>
+                               <td>2081-13-18</td>
+                                <td>
+                                    <a class="btn btn-primary btn-sm text-white">View</a>
+                                    <a class="btn btn-success btn-sm text-white">Edit</a>
+                                    <form action="" method="post" style="display:inline-block">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger btn-sm " onclick="return confirm('Are you sure?')">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
